c7b4873b1fe32c452c7fd6e67b042ffeb6a219ef
ehsyb <ehsyb@naver.com>
2020-05-06 13:26:42 +0900

    초기 커밋.

---
+++ cscomment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            String x1, y1;
+
+            Console.WriteLine("x : ");
+            x1 = Console.ReadLine();
+            x = Convert.ToInt32(x1);
+
+            Console.WriteLine("y : ");
+            y1 = Console.ReadLine();
+            y = Convert.ToInt32(y1);
+
+
+     
+
+            if (x > 4)
+            {
+                if(y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
